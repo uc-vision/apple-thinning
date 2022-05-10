80c19e81333ca5ab52cfecd04661e692b4a3771e
Jade1724 <haruka.i1724@gmail.com>
2022-05-10 22:44:14 +1200

    Showed points of Apple

--- Scenes/AppleCluster.gd
+++ Scenes/AppleCluster.gd
@@ -1,8 +1,10 @@
 extends Spatial
 
 onready var score = 0
+onready var remaining_apple_count = 0
 onready var apple_pick_sound_player = $ApplePickSound
 var point
+var MAX_APPLE_NUM_TO_LEAVE = 2
 
 const Point = {
 	HEALTHY_LARGE = 1.5,
@@ -10,20 +12,49 @@ const Point = {
 	DAMAGED = -3,
 }
 
+func _ready():
+	var children = get_children()
+	for child in children:
+		if child.get_groups().has("Apple"):
+			remaining_apple_count += 1
+
 
 func _on_HealthyLargeApple_on_picked():
 	play_apple_picked_sound()
-	score += Point.HEALTHY_LARGE
+	remaining_apple_count -= 1
+	if remaining_apple_count <= MAX_APPLE_NUM_TO_LEAVE:
+		calculate_score()
 
 
 func _on_HealthySmallApple_on_picked():
 	play_apple_picked_sound()
-	score += Point.HEALTHY_SMALL
+	remaining_apple_count -= 1
+	if remaining_apple_count <= MAX_APPLE_NUM_TO_LEAVE:
+		calculate_score()
 	
 
 func _on_DamagedApple_on_picked():
 	play_apple_picked_sound()
-	score += Point.DAMAGED
+	remaining_apple_count -= 1
+	if remaining_apple_count <= MAX_APPLE_NUM_TO_LEAVE:
+		calculate_score()
+		
+func calculate_score():
+	var children = get_children()
+	for child in children:
+		var groups = child.get_groups()
+		if groups.has("Apple") and !child.picked_off:
+			var point
+			if groups.has("HealthyLarge"):
+				point = Point.HEALTHY_LARGE
+			elif groups.has("HealthySmall"):
+				point = Point.HEALTHY_SMALL
+			else:
+				point = Point.DAMAGED
+			
+			score += point
+			child.show_point()
+		
 	
 func play_apple_picked_sound():
 	apple_pick_sound_player.play()
--- scripts/Apple.gd
+++ scripts/Apple.gd
@@ -1,27 +1,17 @@
 extends RigidBody
 
 signal on_picked
-var picked_off = false
-#var point
-#
-#const Point = {
-#	HEALTHY_LARGE = 1.5,
-#	HEALTHY_SMALL = 1,
-#	DAMAGED = -3,
-#}
+onready var picked_off = false
 
 func _ready():
-	var apple_cluster = get_parent()
-	apple_cluster.connect("on_picked", self, "_on_Apple_on_picked")
-#	var name = get_tree().current_scene.get_name()
-#	if name == "HealthyLargeApple":
-#		point = Point.HEALTHY_LARGE
-#	elif name == "HealthySmallApple":
-#		point = Point.HEALTHY_SMALL
-#	else:
-#		point = Point.DAMAGED
+	pass
+	
 
 func picked_up():
 	if !picked_off:
 		emit_signal("on_picked")
 		picked_off = true
+		
+func show_point():
+	if !picked_off:
+		get_node("PointNode").set_visible(true)
