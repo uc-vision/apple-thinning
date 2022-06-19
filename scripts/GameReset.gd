56cfc5c0c997a784bfed24ad490b04bd5ab46153
Jade1724 <haruka.i1724@gmail.com>
2022-06-19 19:06:01 +1200

    Created the Level1 scene and instantiated in the game to be loaded. Updated the GameReset script

--- scripts/GameReset.gd
+++ scripts/GameReset.gd
@@ -4,5 +4,13 @@ func interact():
 	reset_game()
 	
 func reset_game():
+	# Beep sound for debugging purpose
 	get_tree().root.get_node("Game/AudioStreamPlayer").play()
-	get_tree().change_scene("res://Game.tscn")
+	
+	# Destroy the current level and load a new level
+	var world = get_tree().root.get_node("Game/Levels")
+	var current_level = world.get_child(0)
+	current_level.queue_free()
+	
+	var new_level = load("res://Levels/Level1.tscn").instance()
+	world.add_child(new_level)
