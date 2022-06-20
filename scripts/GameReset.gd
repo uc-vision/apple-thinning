5a5602e87b5f1bf4c42d76eff95f889c28eb0503
Jade1724 <haruka.i1724@gmail.com>
2022-06-20 21:23:08 +1200

    Added a timer to prevent resetting the level multiple times in an attempt

--- scripts/GameReset.gd
+++ scripts/GameReset.gd
@@ -1,7 +1,21 @@
 extends RigidBody
 
+# A timer to avoid resetting game multiple times in one attempt
+const WAIT_TIME  = 2
+onready var timer = $GameResetTimer
+
+func _ready():
+	# Timer setup. No repeat, counts down 2 seconds, and start
+	timer.set_one_shot(true)
+	timer.set_wait_time(WAIT_TIME)
+	timer.start()
+
 func interact():
-	reset_game()
+	# The game reset valid after the timer's remaining time reaches 0
+	if timer.get_time_left() == 0:
+		reset_game()
+		# Restart the timer
+		timer.start()
 	
 func reset_game():
 	# Beep sound for debugging purpose
