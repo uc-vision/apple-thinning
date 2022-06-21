2e4bcf5748a72f8fb10b8d0552b0751a3418035e
Jade1724 <haruka.i1724@gmail.com>
2022-06-22 00:05:49 +1200

    Code documentation and deleted the statically instanced apple clusters as the dynamic spawning is implemented

--- scripts/AppleTree.gd
+++ scripts/AppleTree.gd
@@ -19,13 +19,21 @@ func _ready():
 
 # Find all the spawn path on a tree. Different trees may have different numbers of branch. So the number of path is dynamic.
 	for child in get_children():
+		
+		# Get the spawn paths on the tree
 		if 'ClusterSpawnPath' in child.get_groups():
+			
+			# Random number of apples per branch is spawned 
 			for i in range(rng.randi_range(1, MAX_CLUSTER_PER_BRANCH)):
-				cluster_spawn_location = child.get_child(0)
-				cluster_spawn_location.unit_offset = rng.randf()
 				
+				# Get the spawning location node
+				cluster_spawn_location = child.get_child(0)
+				# Give a random offset of the spawning location
+				cluster_spawn_location.unit_offset = rng.randf()
+				# Pick a type of apple cluster to spawn randomly
 				cluster_type = rng.randi() % 3
-#
+
+				# Instantiate the apple cluster
 				if cluster_type == 0:
 					var apple_cluster_a_instance = apple_cluster_a.instance()
 					apple_cluster_a_instance.initialize(cluster_spawn_location.translation)
