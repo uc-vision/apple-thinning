c04dcdee5f2ff01f0c41c8e15934b8c711ce93fd
Jade1724 <haruka.i1724@gmail.com>
2022-06-22 00:01:19 +1200

    Implemented randomised dynamic apple cluster spawning on the apple tree branch

--- scripts/AppleTree.gd
+++ scripts/AppleTree.gd
@@ -4,6 +4,8 @@ onready var apple_cluster_a = preload("res://Scenes/AppleCluster_TypeA.tscn")
 onready var apple_cluster_b = preload("res://Scenes/AppleCluster_TypeB.tscn")
 onready var apple_cluster_c = preload("res://Scenes/AppleCluster_TypeC.tscn")
 onready var rng = RandomNumberGenerator.new()
+onready var cluster_type
+onready var cluster_spawn_location
 
 enum CLUSTER_TYPE {
 	A,
@@ -14,30 +16,25 @@ const MAX_CLUSTER_PER_BRANCH = 5
 const NUM_BRANCH = 3
 
 func _ready():
-	
-	var cluster_instance = apple_cluster_a.instance()
-	var cluster_spawn_location = $AppleClusterSpawnPath1/PathFollow
-	cluster_instance.initialize(cluster_spawn_location.translation)
-	add_child(cluster_instance)
-	
-	
-	
-#	# Find all the spawn path on a tree. Different trees may have different numbers of branch. So the number of path is dynamic.
-#	for child in get_children():
-#		if 'ClusterSpawnPath' in child.get_groups():
-#			for i in range(rng.randi() % MAX_CLUSTER_PER_BRANCH):
-#				var cluster_spawn_location = child.get_child(0)
-#				cluster_spawn_location.unit_offset = rng.randf()
+
+# Find all the spawn path on a tree. Different trees may have different numbers of branch. So the number of path is dynamic.
+	for child in get_children():
+		if 'ClusterSpawnPath' in child.get_groups():
+			for i in range(rng.randi_range(1, MAX_CLUSTER_PER_BRANCH)):
+				cluster_spawn_location = child.get_child(0)
+				cluster_spawn_location.unit_offset = rng.randf()
+				
+				cluster_type = rng.randi() % 3
 #
-#				var cluster_type = randi() % 3
-#				get_node("AudioStreamPlayer3D").play()
-#				match cluster_type:
-#					CLUSTER_TYPE.A:
-#						add_child(apple_cluster_a.instance())
-#						apple_cluster_a.initialize(cluster_spawn_location.translation)
-#					CLUSTER_TYPE.B:
-#						add_child(apple_cluster_b.instance())
-#						apple_cluster_b.initialize(cluster_spawn_location.translation)
-#					CLUSTER_TYPE.C:
-#						add_child(apple_cluster_c.instance())
-#						apple_cluster_c.initialize(cluster_spawn_location.translation)
+				if cluster_type == 0:
+					var apple_cluster_a_instance = apple_cluster_a.instance()
+					apple_cluster_a_instance.initialize(cluster_spawn_location.translation)
+					add_child(apple_cluster_a_instance)
+				elif cluster_type == 1:
+					var apple_cluster_b_instance = apple_cluster_b.instance()
+					apple_cluster_b_instance.initialize(cluster_spawn_location.translation)
+					add_child(apple_cluster_b_instance)
+				else:
+					var apple_cluster_c_instance = apple_cluster_c.instance()
+					apple_cluster_c_instance.initialize(cluster_spawn_location.translation)
+					add_child(apple_cluster_c_instance)
