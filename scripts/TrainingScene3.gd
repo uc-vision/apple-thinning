1d9faf036f891c53d24ffceaf5e2d0e82a09bab6
GreyNZ <grey_harris@hotmail.com>
2022-08-23 20:59:28 +1200

    traning scene 3 containts a demo of video player

---
+++ scripts/TrainingScene3.gd
@@ -0,0 +1,37 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+#extends VideoPlayer
+#func _process(delta):
+#    var isPlaying = is_playing()
+#    if isPlaying == false:
+#        play()
+#
+#    pass
+#
+#func _ready():
+#    # Called every time the node is added to the scene.
+#    # Initialization here
+#    set_process(true)
+#
+#
+#    set_autoplay(true)
+#    print('Autoplay set to true')
+#
+#    print(has_autoplay())
+#    pass
