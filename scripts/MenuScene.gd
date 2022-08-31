5607dc954e13285328f107e67e52a594a1a3f57e
GreyNZ <grey_harris@hotmail.com>
2022-09-01 04:13:58 +1200

    added wait timer on main menu

--- scripts/MenuScene.gd
+++ scripts/MenuScene.gd
@@ -6,11 +6,14 @@ signal play_training_game_mode
 signal play_time_attack_game_mode
 signal play_tutorial_game_mode
 
+var signal_sent = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	choose_level_dialog.connect("play_training_game_pressed", self, "_on_NoTimeLimit_pressed")
-	choose_level_dialog.connect("play_time_attack_game_pressed", self, "_on_TimeAttack_pressed")
-	choose_level_dialog.connect("play_tutorial_game_pressed", self, "_on_Tutorial_pressed")
+	pass
+
+func play_button_sound():
+	$ButtonPressSoundPlayer.play()
 
 func set_player(player):
 	# Add the player to MenuScene with human readable name
@@ -20,11 +23,25 @@ func set_player(player):
 	$ARVROrigin/RightHand/ScoreAndComboWatch.set_visible(false)
 
 func _on_NoTimeLimit_pressed():
-	emit_signal("play_training_game_mode")
+	if not signal_sent:
+		emit_signal("play_training_game_mode")
+		play_button_sound()
+		signal_sent = true
 	
 func _on_TimeAttack_pressed():
-	emit_signal("play_time_attack_game_mode")
+	if not signal_sent:
+		emit_signal("play_time_attack_game_mode")
+		play_button_sound()
+		signal_sent = true
 
 func _on_Tutorial_pressed():
-	get_tree().root.get_node("Game/AudioStreamPlayer").play()
-	emit_signal("play_tutorial_game_mode")
+	if not signal_sent:
+		get_tree().root.get_node("Game/AudioStreamPlayer").play()
+		emit_signal("play_tutorial_game_mode")
+		play_button_sound()
+		signal_sent = true
+
+func _on_TimerButtonLock_timeout():
+	choose_level_dialog.connect("play_training_game_pressed", self, "_on_NoTimeLimit_pressed")
+	choose_level_dialog.connect("play_time_attack_game_pressed", self, "_on_TimeAttack_pressed")
+	choose_level_dialog.connect("play_tutorial_game_pressed", self, "_on_Tutorial_pressed")
