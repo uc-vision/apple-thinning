4003d5fbd81b406733fe9d7b3d34863e66c0fa29
Haruka Ichinose <haruka.i1724@gmail.com>
2022-10-13 20:50:27 +1300

    Finished implementing changing hand color when entering Non_Interactable_Box.

--- scripts/Non_Interactable_Box.gd
+++ scripts/Non_Interactable_Box.gd
@@ -1,16 +1,28 @@
-extends Area
+extends Spatial
+
+onready var BlueShader = preload("res://Assets/Meshes/MaterialBlue.material")
+onready var GreyShader = preload("res://Assets/Meshes/MaterialRed.material")
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_Area_area_entered(area):
+	
+	if area.get_parent().get_name() == "RightHand":
+		area.get_node("../HandContainer/HandModel/Armature/Skeleton/r_handMeshNode").set_surface_material(0, GreyShader)
+	elif area.get_parent().get_name() == "LeftHand":
+		area.get_node("../HandContainer/HandModel/Armature/Skeleton/l_handMeshNode").set_surface_material(0, GreyShader)
+	pass # Replace with function body.
+
+
+
+func _on_Area_area_exited(area):
+	
+	if area.get_parent().get_name() == "RightHand":
+		area.get_node("../HandContainer/HandModel/Armature/Skeleton/r_handMeshNode").set_surface_material(0, BlueShader)
+	elif area.get_parent().get_name() == "LeftHand":
+		area.get_node("../HandContainer/HandModel/Armature/Skeleton/l_handMeshNode").set_surface_material(0, BlueShader)
+	pass # Replace with function body.
